data-1 : refactor tokenizer code, fix authors' policy gradient implementation (should sum over time, but avg over episodes)

dataloader.py
@@ -1,111 +1,29 @@
 import numpy as np
 from nltk import word_tokenize
 from collections import Counter
-
-class VocabDictionary():
-    def __init__(self, data_fp, max_seq_length=20, character_level_model_bool=False, drop_freq_thresh=10):
-
-        self.character_level_model_bool = character_level_model_bool
-        self.counter = Counter()
-        self.max_seq_length = max_seq_length
-        self.drop_freq_thresh = drop_freq_thresh
-
-        self.go_token = '\x01'
-        self.unk_token = '\x02'
-        self.pad_token = '\x03'
-        self.eos_token = '\x04'
-
-        self.special_tokens = [self.go_token, self.unk_token, self.pad_token, self.eos_token]
-
-        def _char_tokenize(line):
-            line_tokens_list = [c for c in line if ord(c) < 128]
-            return line_tokens_list
-
-        def _word_tokenize(line):
-            line_tokens_list = word_tokenize(line)
-            return line_tokens_list
-
-        def tokenizerFactory():
-            return _char_tokenize if self.character_level_model_bool else _word_tokenize
-
-        self.tokenizer = tokenizerFactory()
-
-        with open(data_fp, 'r') as f:
-            for line in f:
-                line = line.strip().split("\t")[0]
-                line_tokens_list = self.tokenizer(line)
-                self.counter.update(line_tokens_list)
-
-        special_tokens_in_original_data = set(self.special_tokens).intersection(list(self.counter.keys()))
-        assert len(special_tokens_in_original_data) == 0
-
-        self.counter = Counter({k: v for k, v in self.counter.items() if v > self.drop_freq_thresh})
-
-        self.ordered_vocab_list = []
-        self.ordered_vocab_list.append(self.go_token)  # _GO token must be index 0
-        self.ordered_vocab_list.append(self.unk_token)  # _UNK token must be index 1
-        self.ordered_vocab_list.append(self.pad_token)  # _PAD token must be index 2
-        self.ordered_vocab_list.append(self.eos_token)  # _EOS token must be index 3
-        self.ordered_vocab_list.extend([
-            x for x in self.special_tokens if x not in [self.go_token, self.unk_token, self.pad_token, self.eos_token]
-          ]
-        )
-        self.ordered_vocab_list.extend(sorted(list(self.counter.keys())))
-
-        self.vocab_dict = {w: i for i, w in enumerate(self.ordered_vocab_list, 0)}
-        self.int_to_token_dict = {i: w for i, w in enumerate(self.ordered_vocab_list, 0)}
-
-    def lookup(self, token):
-        return self.vocab_dict[token] if token in self.vocab_dict else self.vocab_dict[self.unk_token]
-
-    def reverse_lookup(self, word_id):
-        return self.int_to_token_dict[word_id]
-
-    def get_length(self):
-        return len(self.vocab_dict)
-
+from utils.sequence_extractor import SequenceExtractor
 
 class Gen_Dataloader():
-    def __init__(self, batch_size, vocab_dictionary=None,
-                 max_seq_length=20, min_seq_length=5, character_level_model_bool=False):
+    def __init__(self, vocab_dict, dataset_reader, batch_size):
+
+        self.vocab_dict = vocab_dict
+        self.dataset_reader = dataset_reader
         self.batch_size = batch_size
-        self.token_stream = []
-        self.vocab_dictionary = vocab_dictionary
-        self.max_seq_length = max_seq_length
-        self.min_seq_length = min_seq_length
-        self.character_level_model_bool = character_level_model_bool
 
-        def _char_tokenize(line):
-            line_tokens_list = [c for c in line if ord(c) < 128]
-            return line_tokens_list
-
-        def _word_tokenize(line):
-            line_tokens_list = word_tokenize(line)
-            return line_tokens_list
-
-        def tokenizerFactory():
-            return _char_tokenize if self.character_level_model_bool else _word_tokenize
-
-        self.tokenizer = tokenizerFactory()
-
-    def create_batches(self, data_file):
         self.token_stream = []
 
-        with open(data_file, 'r') as f:
-            for line in f:
-                line = line.strip().split("\t")[0]
-                line = line.split() if self.vocab_dictionary is None else self.tokenizer(line)
-                parse_line = [int(x) if self.vocab_dictionary is None else self.vocab_dictionary.lookup(x)
-                              for x in line]
+    def create_batches(self):
 
-                if len(parse_line) == self.max_seq_length:
-                    self.token_stream.append(parse_line)
-                elif (self.vocab_dictionary is not None) and len(parse_line) < self.max_seq_length and len(parse_line) >= self.min_seq_length:
-                    pad_token = self.vocab_dictionary.pad_token
-                    pad_int = self.vocab_dictionary.lookup(pad_token)
-                    pad_len = self.max_seq_length - len(parse_line)
-                    parse_line.extend([pad_int for _ in range(pad_len)])
-                    self.token_stream.append(parse_line)
+        self.dataset_reader.reprocess()
+
+        textcol_series = self.dataset_reader.standardized_df[self.dataset_reader.text_colname]
+        textcol_series = textcol_series.apply(
+            lambda line: [self.vocab_dict.lookup(x) for x in self.dataset_reader.seq_extractor.tokenize(line)]
+        )
+
+        self.token_stream = textcol_series.tolist()
+        self.token_stream = [token_ints_list for token_ints_list in self.token_stream
+                             if len(token_ints_list) == self.dataset_reader.max_seq_length]
 
         shuffle_indices = np.random.permutation(np.arange(len(self.token_stream)))
         self.token_stream = np.array(self.token_stream)[shuffle_indices]
@@ -125,68 +43,41 @@ class Gen_Dataloader():
 
 
 class Dis_Dataloader():
-    def __init__(self, batch_size, vocab_dictionary=None,
-                 max_seq_length=20, min_seq_length=5, character_level_model_bool=False):
+    def __init__(self, vocab_dict, positive_dataset_reader, negative_dataset_reader, batch_size):
+
+        self.vocab_dict = vocab_dict
+        self.positive_dataset_reader = positive_dataset_reader
+        self.negative_dataset_reader = negative_dataset_reader
         self.batch_size = batch_size
-        self.sentences = np.array([])
-        self.labels = np.array([])
-        self.vocab_dictionary = vocab_dictionary
-        self.max_seq_length = max_seq_length
-        self.min_seq_length = min_seq_length
-        self.character_level_model_bool = character_level_model_bool
 
-        def _char_tokenize(line):
-            line_tokens_list = [c for c in line if ord(c) < 128]
-            return line_tokens_list
+        self.token_stream = []
 
-        def _word_tokenize(line):
-            line_tokens_list = word_tokenize(line)
-            return line_tokens_list
+    def load_train_data(self):
 
-        def tokenizerFactory():
-            return _char_tokenize if self.character_level_model_bool else _word_tokenize
+        self.positive_dataset_reader.reprocess()
+        self.negative_dataset_reader.reprocess()
 
-        self.tokenizer = tokenizerFactory()
+        # normalize as ints
+        positive_examples_series = self.positive_dataset_reader.standardized_df[self.positive_dataset_reader.text_colname]
+        positive_examples_series = positive_examples_series.apply(
+            lambda line: [self.vocab_dict.lookup(x) for x in self.positive_dataset_reader.seq_extractor.tokenize(line)]
+        )
+        positive_examples = positive_examples_series.tolist()
 
-    def load_train_data(self, positive_file, negative_file):
-        # Load data
-        positive_examples = []
-        negative_examples = []
+        negative_examples_series = self.negative_dataset_reader.standardized_df[self.negative_dataset_reader.text_colname]
+        negative_examples_series = negative_examples_series.apply(
+            lambda line: [self.vocab_dict.lookup(x) for x in self.negative_dataset_reader.seq_extractor.tokenize(line)]
+        )
+        negative_examples = negative_examples_series.tolist()
 
-        with open(positive_file, 'r') as fin:
-            for line in fin:
-                line = line.strip().split("\t")[0]
-                line = line.split() if self.vocab_dictionary is None else self.tokenizer(line)
-                parse_line = [int(x) if self.vocab_dictionary is None else self.vocab_dictionary.lookup(x)
-                              for x in line]
+        # make sure no parsing inconsistencies -- gotta have correct length
+        positive_examples = [token_ints_list for token_ints_list in positive_examples
+                             if len(token_ints_list) == self.positive_dataset_reader.max_seq_length]
 
-                if len(parse_line) == self.max_seq_length:
-                    positive_examples.append(parse_line)
-                elif (self.vocab_dictionary is not None) and len(parse_line) < self.max_seq_length and len(parse_line) >= self.min_seq_length:
-                    pad_token = self.vocab_dictionary.pad_token
-                    pad_int = self.vocab_dictionary.lookup(pad_token)
-                    pad_len = self.max_seq_length - len(parse_line)
-                    parse_line.extend([pad_int for _ in range(pad_len)])
-                    positive_examples.append(parse_line)
+        negative_examples = [token_ints_list for token_ints_list in negative_examples
+                             if len(token_ints_list) == self.negative_dataset_reader.max_seq_length]
 
-        with open(negative_file, 'r') as fin:
-            for line in fin:
-                line = line.strip().split("\t")[0]
-                line = line.split() if self.vocab_dictionary is None else self.tokenizer(line)
-                parse_line = [int(x) if self.vocab_dictionary is None else self.vocab_dictionary.lookup(x)
-                              for x in line]
-
-                if len(parse_line) == self.max_seq_length:
-                    negative_examples.append(parse_line)
-                elif (self.vocab_dictionary is not None) and len(parse_line) < self.max_seq_length:
-                    pad_token = self.vocab_dictionary.pad_token
-                    pad_int = self.vocab_dictionary.lookup(pad_token)
-                    pad_len = self.max_seq_length - len(parse_line)
-                    parse_line.extend([pad_int for _ in range(pad_len)])
-                    negative_examples.append(parse_line)
-
-        # we don't want to permit class imbalances per batch.
-
+        # Also, we don't want to permit class imbalances per batch.
         L = min(len(positive_examples), len(negative_examples))
         assert L > 0
 

discriminator.py
@@ -7,21 +7,23 @@ class Discriminator(object):
     Uses an embedding layer, followed by a convolutional, max-pooling and softmax layer.
     """
 
-    def __init__(self, sequence_length, vocab_size, embedding_size, filter_sizes, num_filters, dropout_keep_prob):
+    def __init__(self, sequence_length, vocab_size, embedding_size, filter_sizes, num_filters,
+                 dropout_keep_prob, learning_rate=1e-4, pad_token=None):
 
         # Placeholders for input, output and dropout
         self.input_x = tf.placeholder(tf.int32, [None, sequence_length], name="input_x")   # token ints
         self.input_y = tf.placeholder(tf.float32, [None, 2], name="input_y")               # one-hot encoded class
         self.training_mode = tf.placeholder(tf.bool, name="training_mode")
         self.dropout_keep_prob = dropout_keep_prob
+        self.learning_rate = learning_rate
+
+        self.pad_token_id = pad_token
 
         with tf.variable_scope('discriminator'):
 
             # Embedding layer
             with tf.device('/cpu:0'), tf.name_scope("embedding"):
-                self.W = tf.Variable(
-                    tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0),
-                    name="W")
+                self.W = tf.Variable(tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0), name="W")
                 self.embedded_chars = tf.nn.embedding_lookup(self.W, self.input_x)
 
             # Create a convolution + maxpool layer for each filter size
@@ -40,10 +42,9 @@ class Discriminator(object):
                         name="conv-filters-of-size-%s" % filter_size
                     )
                     # Maxpooling over the outputs
-                    pool_size = sequence_length - filter_size + 1
                     pooled = tf.layers.max_pooling1d(
                         inputs=conv,
-                        pool_size=pool_size,
+                        pool_size=sequence_length-filter_size+1,
                         strides=1,
                         padding='valid',
                         data_format='channels_last',
@@ -84,6 +85,6 @@ class Discriminator(object):
                 ))
 
         self.params = [param for param in tf.trainable_variables() if 'discriminator' in param.name]
-        d_optimizer = tf.train.AdamOptimizer(1e-4)
+        d_optimizer = tf.train.AdamOptimizer(self.learning_rate)
         grads_and_vars = d_optimizer.compute_gradients(self.loss, self.params, aggregation_method=2)
         self.train_op = d_optimizer.apply_gradients(grads_and_vars)

generator.py
@@ -19,7 +19,8 @@ def log_prob_from_logits(x):
 class Generator(object):
     def __init__(self, num_emb, batch_size, emb_dim, hidden_dim,
                  sequence_length, go_token, eos_token=None, pad_token=None, use_onehot_embeddings=False,
-                 learning_rate=0.01, reward_gamma=0.95):
+                 learning_rate=0.01):
+
         self.num_emb = num_emb
         self.batch_size = batch_size
         self.emb_dim = emb_dim
@@ -34,32 +35,16 @@ class Generator(object):
         self.pad_token_id = pad_token
         self.use_onehot_embeddings = use_onehot_embeddings
 
-        self.learning_rate = tf.Variable(float(learning_rate), trainable=False)
-        self.reward_gamma = reward_gamma
+        self.learning_rate = learning_rate
         self.g_params = []
-        self.d_params = []
         self.temperature = 1.0
         self.grad_clip = 5.0
 
         self.expected_reward = tf.Variable(tf.zeros([self.sequence_length]))
 
         with tf.variable_scope('generator'):
-
-            self.g_embeddings_naive = tf.eye(self.num_emb)
-
-            if self.use_onehot_embeddings == False:
-                self.g_embeddings_naive = tf.Variable(self.init_matrix([self.num_emb, self.emb_dim]))
-                self.g_params.append(self.g_embeddings_naive)
-
-            self.g_embeddings = self.g_embeddings_naive
-            if pad_token is not None:
-                self.g_embeddings = tf.concat([
-                        self.g_embeddings_naive[0:self.pad_token_id, :],
-                        tf.zeros(dtype=tf.float32, shape=[1, self.emb_dim]),
-                        self.g_embeddings_naive[(self.pad_token_id+1):, :]
-                    ],
-                    axis=0
-                )
+            self.g_embeddings = tf.Variable(self.init_matrix([self.num_emb, self.emb_dim]))
+            self.g_params.append(self.g_embeddings)
             self.g_recurrent_unit = self.create_recurrent_unit(self.g_params)  # maps h_tm1 to h_t for generator
             self.g_output_unit = self.create_output_unit(self.g_params)  # maps h_t to o_t (output token logits)
 
@@ -137,53 +122,66 @@ class Generator(object):
         #######################################################################################################
         #  Unsupervised Training
         #######################################################################################################
-        #
-        # we ran the recurrence to get g_logits for each step in the generated samples.
-        #
-        # now we can use
-        # sparse_softmax_cross_entropy_with_logits to give us the negative log prob of each token from the generated samples
-        #
-        # i.e.,
-        #     for a given integer y_t representing some token ID generated at time t,
-        #     sparse_softmax_cross_entropy_with_logits is computes
-        #
-        # -log(p(y_t)) for token y_t.
-        #
-        # and it doesn't sum over anything by default--so it just gives us a tensor of -log(p(y_t))
-        # of shape [batch_size, sequence_length]
-        #
-        # We use the func because of numerical stability issues:
-        #
-        # Note, log(softmax(logits)) == log(exp(logit_for_class_i)) - log(sum_i(exp(logit_for_class_i)))
-        #                            == logit_for_class_i - log(sum_i(exp(logit_for_class_i)))
-        #
-        # so we can't just use logit_for_class_i as our log prob,
-        # because it will be larger than the actual log prob
-        # by log(sum_i(exp(logit_for_class_i)))
-        #
-        # and in particular, log sum exp has numerical stability issues
-        #
-        # The reason to use sparse_softmax_cross_entropy_with_logits is
-        # that it handles numerical stability issues.
-        #
-        self.negative_log_prob_for_generated_tokens = tf.nn.sparse_softmax_cross_entropy_with_logits(
+
+        self.negative_log_prob_for_generated_tokens_by_episode = tf.nn.sparse_softmax_cross_entropy_with_logits(
             logits=self.g_logits,
             labels=self.x
         )
-        self.log_prob_for_generated_tokens = -1 * self.negative_log_prob_for_generated_tokens
+        self.log_prob_for_generated_tokens_by_episode = -1 * self.negative_log_prob_for_generated_tokens_by_episode
 
-        # Tensorflow will compute the gradient of this formula w.r.t. the generator model params,
-        # and in doing so, it will give us a gradient estimate matching those given by the REINFORCE formula.
-        self.g_proxy_objective_for_reinforce = tf.reduce_sum(
-            self.log_prob_for_generated_tokens * self.rewards,
+        # The REINFORCE formula given by equation 7 in the paper applies to one episode with steps t = 1,..., T.
+        # In particular, the formula is a sum over time of a conditional expectation.
+        #
+        # The paper remarks that each conditional expectation can be replaced "with sampling methods".
+        #
+        # It was initially unclear to me whether these sampling methods were
+        # related to the rollouts whose rewards were being computed.
+        #
+        # Indeed, if one runs rollouts for a prefix sequence y_{1,...,t-1}
+        # and then computes the average reward by grouped by the first symbol y_t from each of those rollouts,
+        # we could use those separate monte carlo estimates as part of a multi-term approximation of each conditional expectation.
+        #
+        # However, in Lantao Yu's original implementation, there are 16 rollouts per prefix, there is no branching in the prefixes,
+        # and the rewards from all 16 rollouts is averaged.
+        #
+        # By averaging the rewards in this manner,
+        # he is making it clear that there is only one monte carlo estimate for each generated prefix sequence.
+        #
+        # And correspondingly, there is only one grad[log(p(y_t|y_{1,...,t-1}))] term at timestep t.
+        #
+        # So in conclusion, the current implementation by Lantao Yu does not support rollouts that branch, samples that branch, etc.
+        #
+        # Hence, the "sampling methods" from the paper are, in Lantao Yu's current implementation,
+        # based on only one conditional sample y_t, conditioned on y_{1,...,t-1}.
+        #
+        # (Although, yes, the monte carlo estimated reward is based on 16 samples).
+        #
+        # We will follow the same approach.
+        #
+        self.g_proxy_objective_for_reinforce_by_episode = tf.reduce_sum(
+            self.log_prob_for_generated_tokens_by_episode * self.rewards,  # [batch_size, seq_length] x [batch_size, seq_length]
+            axis=1
+        ) # [batch_size]
+
+        # When we have a batch size > 1, this is akin to having have multiple episodes generated by the same policy.
+        #
+        # In order to obtain the gradient estimate for the policy,
+        # I believe assume we are supposed to *average*, not *sum*.
+        #
+        # NOTE: This is different than Lantao Yu's implementation.
+        #
+        self.g_proxy_objective_for_reinforce = tf.reduce_mean(
+            self.g_proxy_objective_for_reinforce_by_episode,
+            axis=0
         )
 
         # In tensorflow, the optimizer takes steps in the *negative* direction of the gradient:
         #
-        #    params := params - alpha * grad(objective)
+        #    params := params - learning_rate * grad(objective)
         #
-        # Thus, we must wrap objective in a minus sign
-        # in order to get gradient steps in the direction that maximizes the reward.
+        # Thus, we must add a minus sign to the original objective, in order to get a new objective
+        # such that taking steps in the negative direction of the new objective
+        # is equivalent to taking positive steps in the direction of the gradient of the original objective.
         #
         self.g_loss = -1 * self.g_proxy_objective_for_reinforce
 